0092e529519b3a9a71167ad4f183ccdad5904332
Anderson-1119 <anderson.osilva35@senacsp.edu.br>
2025-03-25 14:37:10 -0300

    vetor

---
+++ arrayunidimencional-vetor/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayunidimencional_vetor
+{
+    public partial class frmVetor : Form
+    {
+        public frmVetor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            // matriz de vetor unidimensao, tbm chamado d vetor
+            string[] pecasComputador = { "Mouse", "Teclado", "Monitor", "Gabinete", "Câmera" };
+            //MessageBox.Show(pecasComputador[2]);
+            foreach (string peca in pecasComputador) 
+            MessageBox.Show(peca);
+        }
+    }
+}
